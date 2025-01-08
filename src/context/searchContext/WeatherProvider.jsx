313fad190f2e255ae662a55fe950efefe220c387
Hm Faisal <contact.hmfaisal@gmail.com>
2025-01-08 10:05:56 +0600

    setup weather history

--- src/context/searchContext/WeatherProvider.jsx
+++ src/context/searchContext/WeatherProvider.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import WeatherContext from "./WeatherContext";
 import axios from "axios";
+import convertTemp from "../../utils/convertTemp";
 
 const API_KEY = "28c1f1292038dc316c99afca56117407";
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
@@ -10,6 +11,9 @@ const WeatherProvider = ({ children }) => {
   const [location, setLocation] = useState({ city: "dhaka,BD" });
   const [weatherData, setWeatherData] = useState(null);
   const icon = `https://openweathermap.org/img/wn/${weatherData?.weather[0]?.icon}@2x.png`;
+  const [history, setHistory] = useState([]);
+
+  localStorage.setItem("Weather History", JSON.stringify(history));
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -37,6 +41,19 @@ const WeatherProvider = ({ children }) => {
 
     return;
   }, [location]);
+
+  useEffect(() => {
+    setHistory([
+      ...history,
+      {
+        icon,
+        description: weatherData?.weather[0]?.description,
+        city: weatherData?.name,
+        country: weatherData?.sys?.country,
+        temp: convertTemp(weatherData?.main?.temp).celsius.toFixed(),
+      },
+    ]);
+  }, [weatherData, icon]);
 
   const contextValue = {
     location,
--- src/pages/Home/History.jsx
+++ src/pages/Home/History.jsx
@@ -1,35 +1,48 @@
+import { useEffect, useState } from "react";
 import useWeather from "../../hooks/useWeather";
-import convertTemp from "../../utils/convertTemp";
 
 const History = () => {
-  const { weatherData, icon } = useWeather();
+  const { weatherData } = useWeather();
+  const [history, setHistory] = useState([]);
+  useEffect(() => {
+    setHistory(JSON.parse(localStorage.getItem("Weather History")));
+  }, [weatherData]);
+  console.log(history);
   return (
     <>
       <div>
         <h3 className="text-2xl font-bold">Your Weather History</h3>
-        <div className="flex justify-start items-center text-base bg-white rounded mt-2">
-          <div className="text-center text-black mx-auto">
-            {weatherData && (
-              <img
-                src={icon}
-                alt={weatherData?.weather[0]?.description}
-                className="mx-auto"
-              />
-            )}
-          </div>
-          <div className="mr-12 text-black ">
-            <p className="font-bold text-sm">
-              {weatherData?.name}, {weatherData?.sys?.country}
-            </p>
-            <p>{weatherData?.weather[0]?.description}</p>
-          </div>
-          <div className="">
-            <p className="text-4xl min-w-28 text-black">
-              {convertTemp(weatherData?.main?.temp).celsius.toFixed()}
-              <sup>o</sup> C
-            </p>
-          </div>
-        </div>
+        {history.map(
+          (item, i) =>
+            i > 0 && (
+              <div
+                className="flex justify-start items-center text-base bg-white rounded mt-2"
+                key={i}
+              >
+                <div className="text-center text-black mx-auto">
+                  {weatherData && (
+                    <img
+                      src={item.icon}
+                      alt={item?.description}
+                      className="mx-auto"
+                    />
+                  )}
+                </div>
+                <div className="mr-12 text-black ">
+                  <p className="font-bold text-sm">
+                    {item?.city}, {item?.country}
+                  </p>
+                  <p>{item?.description}</p>
+                </div>
+                <div className="">
+                  <p className="text-4xl min-w-28 text-black">
+                    {item.temp}
+                    <sup>o</sup> C
+                  </p>
+                </div>
+              </div>
+            )
+        )}
       </div>
     </>
   );
